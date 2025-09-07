6f68198359b2771a908efd8447ad12e40de33fee
rudraa-ksh <rudrak04sh@gmail.com>
2025-09-07 14:31:27 +0530

    Fix: camelCase

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ app.use(express.json())
 const port = 3000;
 
 const locations = [
-    { id:"DH01", disasterType: "Earthquake",  rangeInKm: 15, latitude: 28.613939, longitude: 77.209023},
-    { id:"DM02", disasterType: "Flood", rangeInKm: 4, latitude: 19.076090, longitude: 72.877426},
-    { id:"DL03", disasterType: "Cyclone", rangeInKm: 3, latitude: 13.082680, longitude: 80.270721},
-    { id:"DH04", disasterType: "Fire",  rangeInKm: 14, latitude: 22.572645, longitude: 88.363892},
-    { id:"DM05", disasterType: "Earthquake", rangeInKm: 8, latitude: 12.971599, longitude: 77.594566},
-    { id:"DH07", disasterType: "Cyclone", rangeInKm: 15, latitude: 18.520430, longitude: 73.856743},
-    { id:"DM08", disasterType: "Fire", rangeInKm: 6, latitude: 23.022505, longitude: 72.571365},
-    { id:"DL09", disasterType: "Earthquake", rangeInKm: 2, latitude: 26.912434, longitude: 75.787270},
-    { id:"DH10", disasterType: "Flood",  rangeInKm: 10, latitude: 26.846695, longitude: 80.946167},
-    { id:"DM11", disasterType: "Cyclone", rangeInKm: 7, latitude: 21.145800, longitude: 79.088158},
-    { id:"DL12", disasterType: "Fire", rangeInKm: 3, latitude: 22.719568, longitude: 75.857727},
-    { id:"DH13", disasterType: "Earthquake",  rangeInKm: 11, latitude: 23.259933, longitude: 77.412613},
-    { id:"DM14", disasterType: "Flood", rangeInKm: 5, latitude: 17.686816, longitude: 83.218483},
-    { id:"DL15", disasterType: "Cyclone", rangeInKm: 2, latitude: 25.594095, longitude: 85.137566}
+    { id:"DH01", type: "Earthquake",  rangeInKm: 15, latitude: 28.613939, longitude: 77.209023},
+    { id:"DM02", type: "Flood", rangeInKm: 4, latitude: 19.076090, longitude: 72.877426},
+    { id:"DL03", type: "Cyclone", rangeInKm: 3, latitude: 13.082680, longitude: 80.270721},
+    { id:"DH04", type: "Fire",  rangeInKm: 14, latitude: 22.572645, longitude: 88.363892},
+    { id:"DM05", type: "Earthquake", rangeInKm: 8, latitude: 12.971599, longitude: 77.594566},
+    { id:"DH07", type: "Cyclone", rangeInKm: 15, latitude: 18.520430, longitude: 73.856743},
+    { id:"DM08", type: "Fire", rangeInKm: 6, latitude: 23.022505, longitude: 72.571365},
+    { id:"DL09", type: "Earthquake", rangeInKm: 2, latitude: 26.912434, longitude: 75.787270},
+    { id:"DH10", type: "Flood",  rangeInKm: 10, latitude: 26.846695, longitude: 80.946167},
+    { id:"DM11", type: "Cyclone", rangeInKm: 7, latitude: 21.145800, longitude: 79.088158},
+    { id:"DL12", type: "Fire", rangeInKm: 3, latitude: 22.719568, longitude: 75.857727},
+    { id:"DH13", type: "Earthquake",  rangeInKm: 11, latitude: 23.259933, longitude: 77.412613},
+    { id:"DM14", type: "Flood", rangeInKm: 5, latitude: 17.686816, longitude: 83.218483},
+    { id:"DL15", type: "Cyclone", rangeInKm: 2, latitude: 25.594095, longitude: 85.137566}
 ];
 
 function getRandomDisasters() {
@@ -32,7 +32,7 @@ function getRandomDisasters() {
     return shuffled.slice(0, randomCount);
 }
 
-app.get('/api/disasters', (req, res) => {
+app.get('/api/v1//disasters', (req, res) => {
     const disasters = getRandomDisasters();
     
     res.json({
@@ -40,9 +40,5 @@ app.get('/api/disasters', (req, res) => {
         disasters: disasters,
     });
 });
-
-app.get("/", (req,res)=>{
-    res.send("Hello Rudra!")
-})
 
 app.listen(port,()=>console.log(`Server running on PORT ${port}`));
